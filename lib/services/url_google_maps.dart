23b4fd5b3b865f9145da03be5f5ed3f1921ed6dd
sumithgs <sumithgs2000@gmail.com>
2021-03-21 12:00:07 +0530

    modification

--- lib/services/url_google_maps.dart
+++ lib/services/url_google_maps.dart
@@ -1,9 +1,9 @@
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 Future<void> openMap(
     double initlat, double initlng, double finallat, double finallng) async {
   String googleUrl =
-      // 'https://www.google.com/maps/search/?api=AIzaSyBN7dPGGCRYBMwxQ1Ran7wwOekF1QpJmuE&query=$lat,$lng';
       'https://www.google.com/maps/dir/?api=1&origin=$initlat,$initlng&destination=$finallat,$finallng';
   if (await canLaunch(googleUrl)) {
     await launch(googleUrl);
